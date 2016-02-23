1e104f861471e59d800990bfa4c8413f816e9cd5
Sri Velicheti <velicheti.harsha@gmail.com>
2016-02-23 14:05:47 -0500

    Coin Change sol 2

--- DP/CoinChange/CoinChange/Solution.cs
+++ DP/CoinChange/CoinChange/Solution.cs
@@ -7,17 +7,143 @@ using System.Threading.Tasks;
 
 namespace CoinChange
 {
+
+    public class CoinCollection
+    {
+        private IDictionary<int, int> _collection = new Dictionary<int, int>();
+        private int _amount;
+        public CoinCollection(int amount)
+        {
+            _amount = amount;
+        }
+
+        public void Update(CoinCollection collection)
+        {
+            foreach (var kvp in collection.Coins)
+            {
+                this.AddToCollection(kvp.Key, kvp.Value);
+            }
+        }
+        public IDictionary<int, int> Coins
+        {
+            get
+            {
+                return _collection;
+            }
+        }
+        public CoinCollection(int amount, int coin, int number)
+        {
+            _amount = amount;
+            AddToCollection(coin, number);
+        }
+        public void AddToCollection(int coin, int number)
+        {
+            if (_collection.ContainsKey(coin))
+            {
+                _collection[coin] = _collection[coin] + number;
+            }
+            else
+                _collection[coin] = number;
+        }
+
+        public CoinCollection(int amount, CoinCollection collection)
+        {
+            _amount = amount;
+            Update(collection);
+        }
+
+        public CoinCollection(int amount, int coin, int number ,CoinCollection collection)
+        {
+            _amount = amount;
+            Update(collection);
+            AddToCollection(coin, number);
+        }
+
+        public override string ToString()
+        {
+            var disp = string.Empty;
+            foreach (var kvp in _collection.OrderBy(x => x.Key))
+            {
+                disp = disp + $"Den-{kvp.Key}_Num-{kvp.Value}||";
+            }
+            return disp;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != typeof(CoinCollection))
+                return false;
+            if (obj == null)
+                return false;
+            var x = this;
+            var y = (CoinCollection)obj;
+            var b = x.Coins.All(c =>
+            {
+                return y.Coins.Any(z => z.Key == c.Key && z.Value == c.Value);
+            });
+            return b;
+        }
+
+        public int GetHashCode(CoinCollection obj)
+        {
+            return base.GetHashCode();
+        }
+    }
+
+    public class CollectionComparer : IEqualityComparer<CoinCollection>
+    {
+        //public int Compare(CoinCollection x, CoinCollection y)
+        //{
+           
+        //}
+
+        public bool Equals(CoinCollection x, CoinCollection y)
+        {
+            var b = x.Coins.All(c =>
+            {
+                return y.Coins.Any(z => z.Key == c.Key && z.Value == c.Value);
+            });
+            return b;
+        }
+
+        public int GetHashCode(CoinCollection obj)
+        {
+            return base.GetHashCode();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void AddOrUpdate(this Dictionary<int, List<CoinCollection>> dic, int amount, CoinCollection collection)
+        {
+            if (dic.ContainsKey(amount))
+            {
+                if(!dic[amount].Exists(x => x.Equals(collection)))
+                    dic[amount].Add(collection);
+            }
+            else
+            {
+                var coll = new List<CoinCollection>();
+                coll.Add(collection);
+                dic[amount] = coll;
+            }
+        }
+    }
+
     class Solution
     {
+
         protected static TextReader reader;
         protected static TextWriter writer;
-        static Dictionary<int,int> _cache = new Dictionary<int, int>();
+        static Dictionary<int, int> _cache = new Dictionary<int, int>();
 
         static Dictionary<int, List<string>> _secCache = new Dictionary<int, List<string>>();
+        static Dictionary<int, List<CoinCollection>> _collCache = new Dictionary<int, List<CoinCollection>>();
         private static int[] _coins;
         static void Main(string[] args)
         {
 #if DEBUG
+
             reader = new StreamReader("..\\..\\input.txt");
             writer = Console.Out;
             // writer = new StreamWriter("..\\..\\output.txt");
@@ -27,22 +153,23 @@ namespace CoinChange
 #endif
             Solve();
 #if DEBUG
-            //Console.WriteLine("Completed");
-            //Console.ReadLine();
+            Console.WriteLine("Completed");
+            Console.ReadLine();
 #endif
         }
 
         static void Solve()
         {
-            _cache.Add(0,0);
-            var amount = Convert.ToInt32(reader.ReadLine().Split(' ')[0]);
+            _cache.Add(0, 0);
+            var amount = Convert.ToInt32(reader.ReadLine().Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries)[0]);
 
-            _coins = reader.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+            _coins = reader.ReadLine().Split(' ').Where(x => !string.IsNullOrEmpty(x)).Select(x => Convert.ToInt32(x)).ToArray();
             foreach (var c in _coins)
             {
                 //_cache.Add(c,1);
             }
-            writer.WriteLine(GetNumberOfDenominations(amount));
+            GetNumberOfDenominations(amount);
+            writer.WriteLine( _collCache.ContainsKey(amount)? _collCache[amount].Distinct(new CollectionComparer()).Count():0);
             writer.Flush();
 #if DEBUG
             writer.Close();
@@ -64,16 +191,14 @@ namespace CoinChange
                 {
                     var total = 0;
                     List<string> currentCoins = new List<string>();
-                    HashSet<int> _considered = new HashSet<int>();
-                    int maxFigured = 0;
                     foreach (var coin in _coins)
                     {
-                        if (!_considered.Contains(coin))
                         {
                             if (coin == amount)
                             {
                                 currentCoins.Add(coin.ToString());
-                                total++;
+                                _collCache.AddOrUpdate(amount, new CoinCollection(amount, coin, 1));
+                                //total++;
                             }
                             else if (coin <= amount)
                             {
@@ -81,20 +206,20 @@ namespace CoinChange
                                 {
                                     var toFigure = amount - coin;
                                     var den = GetNumberOfDenominations(toFigure);
-                                    _considered.Add(toFigure);
                                     if (den > 0)
                                     {
                                         total = total + den;
-                                        foreach (var s in _secCache[toFigure])
+                                        var perms = _collCache[toFigure];
+                                        foreach (var coll in perms)
                                         {
-                                            var f = ( s + coin.ToString()).
+                                            _collCache.AddOrUpdate(amount, new CoinCollection(amount, coin, 1, coll));
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                    _cache[amount] = total;
+                    _cache[amount] = _collCache.ContainsKey(amount) ? _collCache[amount].Count :0;
                     return total;
                 }
             }
