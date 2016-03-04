9620407cacbc45d7379e88b68232fd1b51d6003b
Sri Velicheti <velicheti.harsha@gmail.com>
2016-03-04 17:08:55 -0500

    Stock maximize

---
+++ DP/StockMaximize/StockMaximize/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution
+{
+    protected static TextReader reader;
+    protected static TextWriter writer;
+    static void Main(string[] args)
+    {
+#if DEBUG
+
+        reader = new StreamReader("..\\..\\input.txt");
+         writer = Console.Out;
+        //writer = new StreamWriter("..\\..\\output.txt", false);
+
+#else
+            //reader = Console.In;
+        reader = new StreamReader("..\\..\\input.txt");
+        writer = Console.Out; //  new StreamWriter(Console.OpenStandardOutput());
+#endif
+
+        int noOfTestCases = int.Parse(reader.ReadLine());
+        for (int z = 0; z < noOfTestCases; z++)
+        {
+            Solve();
+        }
+#if DEBUG
+        Console.WriteLine("Completed");
+        Console.ReadLine();
+#endif
+        Console.WriteLine("Completed");
+        Console.ReadLine();
+    }
+    static void Solve()
+    {
+        var noOfDays = int.Parse(reader.ReadLine());
+        var options = new int[noOfDays];
+        var dailyPrices = reader.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+        for (int i = 1; i < noOfDays; i++)
+        {
+            if (dailyPrices[i] > dailyPrices[i - 1])
+            {
+                options[i] = -1;
+                int j = i-1;
+                int currentPrice = dailyPrices[i];
+                do
+                {
+                    if (dailyPrices[j] < currentPrice)
+                        options[j] = 1;
+                    else
+                        break;
+                    j--;
+                } while (j >= 0);
+            }
+        }
+        long currentProfit = 0;
+        long currentCost = 0;
+        long accumulatedShares = 0;
+        for (int i = 0; i < noOfDays; i++)
+        {
+            if (options[i] == 1)
+            {
+                accumulatedShares++;
+                currentCost = currentCost + dailyPrices[i];
+            }
+            else if (options[i] == -1)
+            {
+                currentProfit = currentProfit + ((accumulatedShares * dailyPrices[i]) - currentCost);
+                accumulatedShares = 0;
+                currentCost = 0;
+            }
+        }
+        writer.WriteLine(currentProfit);
+        writer.WriteLine(string.Join(" ", options));
+    }
+}
---
+++ DP/StockMaximize/StockMaximize2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution
+{
+    protected static TextReader reader;
+    protected static TextWriter writer;
+    static void Main(string[] args)
+    {
+#if DEBUG
+
+        reader = new StreamReader("..\\..\\input.txt");
+         writer = Console.Out;
+        //writer = new StreamWriter("..\\..\\output.txt", false);
+
+#else
+        reader = Console.In;
+        writer = Console.Out;
+#endif
+        int noOfTestCases = int.Parse(reader.ReadLine());
+        for (int z = 0; z < noOfTestCases; z++)
+        {
+            Solve();
+        }
+#if DEBUG
+        Console.WriteLine("Completed");
+        Console.ReadLine();
+#endif
+    }
+    static void Solve()
+    {
+        var noOfDays = int.Parse(reader.ReadLine());
+        var options = new int[noOfDays];
+        var dailyPrices = reader.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        var currMax = dailyPrices[noOfDays - 1];
+        options[noOfDays - 1] = -1;
+        for (int i = noOfDays - 2; i >= 0; i--) {
+            var todayPrice = dailyPrices[i];
+            if (todayPrice < currMax)
+            {
+                options[i] = 1;
+            }
+            else if (todayPrice > currMax)
+            {
+                options[i] = -1;
+                currMax = dailyPrices[i];
+            }
+        }
+
+        long currentProfit = 0;
+        long currentCost = 0;
+        long accumulatedShares = 0;
+        for (int i = 0; i < noOfDays; i++)
+        {
+            if (options[i] == 1)
+            {
+                accumulatedShares++;
+                currentCost = currentCost + dailyPrices[i];
+            }
+            else if (options[i] == -1)
+            {
+                currentProfit = currentProfit + ((accumulatedShares * dailyPrices[i]) - currentCost);
+                accumulatedShares = 0;
+                currentCost = 0;
+            }
+        }
+        writer.WriteLine(currentProfit);
+       // writer.WriteLine(string.Join(" ", options));
+    }
+}
