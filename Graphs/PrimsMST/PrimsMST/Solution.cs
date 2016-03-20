55475871cd1d6e626c287cd8e9348b26efef00f9
srivelicheti <velicheti.harsha@gmail.com>
2016-03-20 12:42:24 -0400

    Prims MST

---
+++ Graphs/PrimsMST/PrimsMST/Solution.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace PrimsMST
+{
+    class Solution
+    {
+        protected static TextReader reader;
+        protected static TextWriter writer;
+
+        static void Main(string[] args)
+        {
+#if DEBUG
+            reader = new StreamReader("..\\..\\input.txt");
+            writer = Console.Out;
+            // writer = new StreamWriter("..\\..\\output.txt");
+#else
+            reader = Console.In;
+            writer = new StreamWriter(Console.OpenStandardOutput());
+#endif
+            Solve();
+#if DEBUG
+            Console.WriteLine("Completed");
+            Console.ReadLine();
+#endif
+        }
+
+        static void Solve()
+        {
+            var lens = reader.ReadLine().Split(' ').Select(x=> Convert.ToInt32(x)).ToArray();
+            var nodes = lens[0];
+            var edges = lens[1];
+            var al = new List<Dictionary<int, int>>(nodes);
+            for (int i = 0; i < nodes; i++)
+            {
+                al.Add(new Dictionary<int, int>());
+            }
+            for (int i = 0; i < edges; i++)
+            {
+                var lineParts = reader.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+                var s = lineParts[0] -1;
+                var e = lineParts[1] -1;
+                var w = lineParts[2];
+                var dic = al[s];
+                if (dic.ContainsKey(e))
+                {
+                    var existingWeigh = dic[e];
+                    if (w < existingWeigh)
+                        dic[e] = w;
+                }
+                else
+                    dic.Add(e, w);
+
+                 dic = al[e];
+                if (dic.ContainsKey(s))
+                {
+                    var existingWeigh = dic[s];
+                    if (w < existingWeigh)
+                        dic[s] = w;
+                }
+                else
+                    dic.Add(s, w);
+
+            }
+
+            var start = Convert.ToInt32(reader.ReadLine()) - 1 ;
+
+            Dictionary<int, int> consumedEdges = new Dictionary<int, int>();
+            consumedEdges.Add(start, 0);
+            var nextEdge = GetNextEdgeToConsume(al, consumedEdges);
+            int totalWeight = 0;
+            while (nextEdge != null)
+            {
+                totalWeight = totalWeight + nextEdge.Item3;
+                consumedEdges.Add(nextEdge.Item2, nextEdge.Item3);
+                nextEdge = GetNextEdgeToConsume(al, consumedEdges);
+            }
+            writer.WriteLine(totalWeight);
+
+            writer.Flush();
+#if DEBUG
+            writer.Close();
+#endif
+        }
+
+        static Tuple<int,int,int> GetNextEdgeToConsume(List<Dictionary<int, int>> nodes, Dictionary<int,int> consumedEdges)
+        {
+            var toAddWeight = int.MaxValue;
+            Int32 toAddStart = -1;
+            Int32 toAddEnd = -1;
+            foreach (var key in consumedEdges.Keys)
+            {
+                var d = nodes[key];
+                foreach (var kvp in d.Where(x => !consumedEdges.ContainsKey(x.Key)))
+                {
+                    if (kvp.Value < toAddWeight)
+                    {
+                        toAddWeight = kvp.Value;
+                        toAddStart = key;
+                        toAddEnd = kvp.Key;
+                    }
+                }
+            }
+            if (toAddStart == -1)
+                return null;
+            else
+                return new Tuple<int, int, int>(toAddStart, toAddEnd, toAddWeight);
+        }
+    }
+}
---
+++ Strings/SherlockAndAnagrams/SherlockAndAnagrams/Solution.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SherlockAndAnagrams
+{
+    class Solution
+    {
+        protected static TextReader reader;
+        protected static TextWriter writer;
+
+        static void Main(string[] args)
+        {
+#if DEBUG
+            reader = new StreamReader("..\\..\\input.txt");
+            writer = Console.Out;
+            // writer = new StreamWriter("..\\..\\output.txt");
+#else
+            reader = Console.In;
+            writer = new StreamWriter(Console.OpenStandardOutput());
+#endif
+            var numOfTestCases = Convert.ToInt32(reader.ReadLine());
+            for (int i = 0; i < numOfTestCases; i++)
+            {
+                Solve();
+            }
+
+#if DEBUG
+            Console.WriteLine("Completed");
+            Console.ReadLine();
+#endif
+        }
+
+        private static Dictionary<char, List<int>> indexes;
+        static void Solve()
+        {
+            var str = reader.ReadLine();
+            indexes = new Dictionary<char, List<int>>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                var c = str[i];
+                if (indexes.ContainsKey(c))
+                    indexes[c].Add(i);
+                else
+                    indexes[c] = new List<int>() { i };
+            }
+            writer.WriteLine(SolveSubProblems(str,0,str.Length -1 ));
+            writer.Flush();
+#if DEBUG
+            writer.Close();
+#endif
+        }
+
+        static int SolveSubProblems(string s, int start, int end)
+        {
+            // if (indexes[c].Count(x => x >= start && x <= end) <= 1)
+            int numOfPairs = 0;
+            var visited = new List<char>();
+            for (int i = start; i <= end; i++)
+            {
+                var c = s[i];
+                if (visited.Contains(c))
+                    continue;
+                var tempCount = indexes[c].Count(x => x >= start && x <= end);
+                if (tempCount <= 1)
+                    continue;
+                
+                numOfPairs = numOfPairs + ((tempCount * tempCount - 1) / 2);
+                var curIndexes = indexes[c];
+                for (int j = 0; j < curIndexes.Count - 1; j++)
+                {
+                    var t2 = curIndexes[j];
+                    if(t2 <start || t2 > end)
+                        continue;
+                    for (int z = j + 1; z < curIndexes.Count; z++)
+                    {
+                        var t1 = curIndexes[z];
+                        if(t1 < start || t1 > end)
+                            continue;
+                        if (t1 - t2 == 1)
+                            continue;
+                        else if (t1 - t2 == 2)
+                        {
+                            if (s[t2 + 1] != c)
+                                numOfPairs = numOfPairs + 1;
+                        }
+                        else
+                        {
+                            numOfPairs++;
+                            numOfPairs = numOfPairs + SolveSubProblems2(s, t2 + 1, t1 - 1);
+                        }
+                    }
+                }
+
+                visited.Add(c);
+
+            }
+            return numOfPairs;
+        }
+
+
+        static int SolveSubProblems2(string s, int start, int end)
+        {
+
+           
+            var indices = new Dictionary<char, List<int>>();
+            for (int i = start; i <= end; i++)
+            {
+                var c = s[i];
+                if (indices.ContainsKey(c))
+                    indices[c].Add(i);
+                else
+                    indices[c] = new List<int>() { i };
+            }
+
+            int numOfPairs = 0;
+            var visited = new List<char>();
+            for (int i = start; i <= end; i++)
+            {
+                var c = s[i];
+                if(visited.Contains(c))
+                    continue;
+                if (indices[c].Count() <= 1)
+                    continue;
+                var tempCount = indices[c].Count;
+                numOfPairs = numOfPairs + ((tempCount*tempCount - 1)/2);
+                var curIndexes = indices[c];
+                for (int j = 0; j < curIndexes.Count - 1; j++)
+                {
+                    for (int z = j + 1; z < curIndexes.Count; z++)
+                    {
+                        var t1 = curIndexes[z];
+                        var t2 = curIndexes[j];
+                        if (t1 - t2 == 1)
+                            continue;
+                        else if (t1 - t2 == 2)
+                        {
+                            if(s[t2 +1] != c)
+                                numOfPairs = numOfPairs + 1;
+                        }
+                        else
+                        {
+                            numOfPairs++;
+                            numOfPairs = numOfPairs + SolveSubProblems2(s, t2 + 1, t1 - 1);
+                        }
+                    }
+                }
+
+                visited.Add(c);
+
+            }
+            return numOfPairs;
+        }
+    }
+}
